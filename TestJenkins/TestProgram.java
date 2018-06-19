f507d12abafb5192232562b237cfda28948d6c5d
SAI Deloitte <saiprasad941@gmail.com>
2018-06-19 20:22:10 +0530

    Sample java program

---
+++ TestJenkins/TestProgram.java
@@ -0,0 +1,13 @@
+public class TestProgram{
+	
+	public static void main(String... args){
+		
+		System.out.println("======= Executing TestProgram =====");
+		
+		
+		System.out.println("==== Execution Completed ===");
+		
+	}
+	
+	
+}
